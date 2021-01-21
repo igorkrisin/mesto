e318ca9bf27154110d33720c4cc42a9ab2047433
Igor Krysin <igor_krisin@mail.ru>
2021-01-21 18:35:28 +0300

    Сделать Попап всплвающим

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const popupOpen = document.querySelector('.profile__info-btn');
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup');
 popupOpen.addEventListener("click", function(e) {
+    popup.classList.add('popup_opened')
+});
 
-    console.log(popupOpen);
-})
+const popupClose = document.querySelector('.popup__close');
+popupClose.addEventListener("click", function(e){ 
+    popup.classList.remove('popup_opened')
+});
+
+const freeLike = document.querySelector('.grid-block__item-panel-like');
+if (freeLike.length > 0) {
+    for (let index = 0; index < freeLike.length; index++) {
+    }
+}
+freeLike.addEventListener("click", function(e){
+    freeLike.classList.add('grid-block__item-panel-like_active')
+
+});
+
+console.log(freeLike);
