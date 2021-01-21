bab3af8264b452137a55df7c83d44ac63f287147
Igor Krysin <igor_krisin@mail.ru>
2021-01-21 08:13:07 +0300

    Написать первый  код с js

---
+++ script.js
@@ -0,0 +1,6 @@
+const popupOpen = document.querySelector('.profile__info-btn');
+const popup = document.querySelector('.popup')
+popupOpen.addEventListener("click", function(e) {
+
+    console.log(popupOpen);
+})
