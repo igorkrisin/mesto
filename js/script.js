e56701570812174e0a9c3b646ec55e98ea081dc0
Igor Krysin <igor_krisin@mail.ru>
2021-01-27 08:02:08 +0300

    Правки  коммит 2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const popupOpen = document.querySelector('.profile__info-btn');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 let profilInfoName = document.querySelector('.profile__info-name');
-let containeInputName = document.querySelector('.popup__container-input_name');
+let containeInputName = document.querySelector('.popup__container-input_js_name');
 let profileInfoDescription = document.querySelector('.profile__info-description');
-let popupContainerInputDescription = document.querySelector('.popup__container-input_description')
+let popupContainerInputDescription = document.querySelector('.popup__container-input_js_description')
 let formElement = document.querySelector('.popup__container-content');
 
 function popupOpened(e) {
